70678d5b9ea3d33538b17d0232d4f0312bb373be
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-09-23 11:56:39 +0800

    feat: display users in the table

--- src/pages/UserPage.vue
+++ src/pages/UserPage.vue
@@ -1,14 +1,27 @@
 <template>
   <q-page class="column">
-    <h1>Logged In</h1>
+    <q-table
+      title="Users"
+      row-keys="id"
+      :rows="userStore.getUsers"
+    >
+    </q-table>
+
     <q-btn name="logout" unelevated color="primary" label="Logout" @click="logout" />
   </q-page>
 </template>
 
 <script setup lang="ts">
+import { watchEffect } from 'vue';
 import { useAuthStore } from '../stores/authStore';
+import { useUsersStore } from '../stores/usersStore';
 
 const authStore = useAuthStore();
+const userStore = useUsersStore();
+
+watchEffect(() => {
+  userStore.getAll();
+});
 
 const logout = () => {
   authStore.logout();
