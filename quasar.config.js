8d236de31db23f632572889346d6a66f70b39ac8
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-09-23 11:56:55 +0800

    chore: add host and port value

--- quasar.config.js
+++ quasar.config.js
@@ -96,6 +96,8 @@ module.exports = configure(function(/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
       // https: true
+      host: '0.0.0.0',
+      port: 9000,
       open: false // opens browser window automatically
     },
 
